87d19e0224f77d202d1bb8c090a96c7ea806f2c5
Liam Glennon McColgan <liammccolgan@gmail.com>
2026-03-29 13:15:49 -0400

    finally picked out a name

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dating app</title>
+    <title>Parasocial</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
