3fac0c2a5db8547e4a32f3103013502e50e9450a
Liam Glennon McColgan <liammccolgan@gmail.com>
2026-04-12 23:15:31 -0400

    changed plaintext passwords to hashed passwords, added new accounts, added admin request table

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2026 at 03:48 PM
+-- Generation Time: Apr 13, 2026 at 05:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,9 +42,32 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`account_id`, `username`, `password`, `utype`, `isbot`, `created_at`, `standing`) VALUES
-(1, 'flynn_mctaggart', 'ripntear', 'user', 0, '2026-03-17 14:11:50', NULL),
-(2, 'spartan117', 'pillarofautumn', 'user', 0, '2026-03-29 13:07:00', NULL),
-(3, 'GOD', 'ashbagoba12', 'admin', 0, '2026-03-29 13:39:03', NULL);
+(1, 'flynn_mctaggart', '$2y$10$JotPLyuir4knYjJ4b3APHOj9/mhZyMdDTB.tZ.cwSdj2ZNP85wSh2', 'user', 0, '2026-03-17 14:11:50', NULL),
+(2, 'spartan117', '$2y$10$iLi/dvMQbGq4Zg7uHfZH0.fa/knhjuHmErNPy5ZZ8EX8J4ziKGlRi', 'user', 0, '2026-03-29 13:07:00', NULL),
+(3, 'GOD', '$2y$10$7XYm5THrwhs9NVDcAZCcYObEox4sQYyxQrbugYnC7ivV1yIvOC.EO', 'admin', 0, '2026-03-29 13:39:03', NULL),
+(4, 'Jehova', '$2y$10$QTWu4yjb7608nT58WGgu3.lguyI/wvdQxKrFgf9vKvYKMQH/DELKG', 'admin', 0, '2026-04-12 23:09:56', NULL),
+(5, 'Yahweh', '$2y$10$AePrS/CBSepu9PfPRNozCeR2.NU/tlPX8otUwR3pEs7LcpLWCiNU.', 'user', 0, '2026-04-12 23:10:59', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_requests`
+--
+
+CREATE TABLE `admin_requests` (
+  `request_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `status` enum('pending','approved','denied') NOT NULL DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_requests`
+--
+
+INSERT INTO `admin_requests` (`request_id`, `account_id`, `status`, `created_at`) VALUES
+(1, 4, 'approved', '2026-04-13 03:09:56'),
+(2, 5, 'denied', '2026-04-13 03:10:59');
 
 -- --------------------------------------------------------
 
@@ -95,7 +118,9 @@ CREATE TABLE `profile` (
 
 INSERT INTO `profile` (`profile_id`, `acc_id`, `screenname`, `summary`, `likes`, `dislikes`, `isprivate`) VALUES
 (1, 2, 'master_chief', 'hasn\'t been in a good game for almost 20 years', 'green', 'purple', 0),
-(2, 3, 'GOD', 'GOD', 'GOD', 'anchovies', 0);
+(2, 3, 'GOD', 'GOD', 'GOD', 'anchovies', 0),
+(3, 4, 'Iehova', 'Jehova starts with an I', 'I', 'J', 0),
+(4, 5, 'YHWH', 'oy', 'nothing', 'most things', 0);
 
 --
 -- Indexes for dumped tables
@@ -107,6 +132,13 @@ INSERT INTO `profile` (`profile_id`, `acc_id`, `screenname`, `summary`, `likes`,
 ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `admin_requests`
+--
+ALTER TABLE `admin_requests`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `account_id` (`account_id`);
 
 --
 -- Indexes for table `bot`
@@ -138,7 +170,13 @@ ALTER TABLE `profile`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `admin_requests`
+--
+ALTER TABLE `admin_requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `bot`
@@ -156,11 +194,17 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `admin_requests`
+--
+ALTER TABLE `admin_requests`
+  ADD CONSTRAINT `admin_requests_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`);
 
 --
 -- Constraints for table `bot`
